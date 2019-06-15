97c1f0cb0d578e64f3fb3311552d2e2c3b7644e7
Florian Arndt <post@florianarndt.com>
2019-06-15 19:55:22 +0200

    first version

---
+++ Formula/ssh2av.rb
@@ -0,0 +1,10 @@
+class ssh2av < Formula
+  desc "Writes SSH config file to an Ansible Vault YML variable file"
+  homepage "https://arweb.de"
+  version "0.1"
+  url "https://github.com/flowli/homebrew-ssh2av/raw/master/ssh2av/ssh2av.zip", :using => :curl
+
+  def install
+     bin.install "ssh2av"
+  end
+end
